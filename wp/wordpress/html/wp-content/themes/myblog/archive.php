22cd73b43f7511913f419f3cdeea47c0d2cb4bd3
szq529 <sk1385zq@gmail.com>
2021-11-30 22:33:39 +0900

    add file archive.php

---
+++ wp/wordpress/html/wp-content/themes/myblog/archive.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!-- <html lang="en"> -->
+<html <?php language_attributes(); ?>>
+
+<head>
+
+  <?php get_header(); ?>
+
+</head>
+
+<body>
+  <!-- パーツを使用 -->
+  <?php get_template_part('includes/header'); ?>
+
+
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>category</h1>
+            <!-- テンプレートタグを使用して表示しているページのタイトルを表示するためのもの -->
+            <span class="subheading"><?php wp_title(``); ?></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <!-- 条件分岐 -->
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
+            <div class="post-preview">
+              <!-- 各ページのリンク -->
+              <a href="<?php the_permalink(); ?>">
+                <h2 class="post-title">
+                  <?php the_title(); ?>
+                </h2>
+                <h3 class="post-subtitle">
+                  <?php the_excerpt(); ?>
+                </h3>
+              </a>
+              <p class="post-meta">Posted by
+                <!-- 著者名 -->
+                <?php the_author(); ?>
+                <!-- 作成日 -->
+                on <?php the_time(get_option('date_format')); ?>
+              </p>
+            </div>
+            <hr>
+          <?php endwhile; ?>
+
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+          </div>
+        <?php else : ?>
+          <p>記事が見つかりませんでしたよ！</p>
+        <?php endif; ?>
+      </div>
+      <!-- 分岐終了 -->
+    </div>
+  </div>
+
+  <hr>
+
+  <!-- パーツを使用 -->
+  <?php get_template_part('includes/footer'); ?>
+
+  <!-- パーツに全て含めないのはあるページでは要素を含めいたい、加えたい際に分けた方が扱いやすいかも -->
+
+  <?php get_footer(); ?>
+</body>
+
+</html>
